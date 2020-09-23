7c73fc8614b53ede1185e35891902888aa43acfa
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2020-09-23 21:24:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 export KBUILD_BUILD_USER=fadlyas07
 export ARCH=arm64 && export SUBARCH=arm64
-export LD_LIBRARY_PATH="$(pwd)/proton-clang/lib:$LD_LIBRARY_PATH"
+export LD="proton-clang/bin/ld.lld"
 export PATH="$(pwd)/proton-clang/bin:$PATH"
 export KBUILD_BUILD_TIMESTAMP=$(TZ=Asia/Jakarta date)
 make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1}
 make -j$(nproc) -l$(nproc) ARCH=arm64 O=out \
-CC=clang CROSS_COMPILE=aarch64-linux-gnu- \
-AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy \
-OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1| tee build.log
+CC=clang LD=ld.lld CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image.gz-dtb ]] ; then
     curl -s -X POST "https://api.telegram.org/bot960007819:AAGjqN3UsMFc7iFMkc0Mj8owotH-oJchCag/sendMessage" -d chat_id="784548477" -d text="Test Failed, Please fix it now @fadlyas07!"
     curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot960007819:AAGjqN3UsMFc7iFMkc0Mj8owotH-oJchCag/sendDocument" -F chat_id="784548477"
