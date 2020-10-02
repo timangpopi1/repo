b0a27ebe5238be75191c6b34742c4b0efffa77a4
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2020-10-02 18:44:25 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+export channel_id="-1001482549527"
 export token="1355238694:AAElWMuJhDKoE9Ci6INuD86RXwpo84uTt7c"
-git clone --depth=1 -q https://github.com/crdroidmod/android_prebuilts_clang_host_linux-x86_clang-5799447 push
+git clone --depth=1 https://github.com/crdroidmod/android_prebuilts_clang_host_linux-x86_clang-5799447 push
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r50 gcc
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r50 gcc32
 export KBUILD_BUILD_USER=fadlyas07
@@ -12,12 +13,12 @@ make -j$(nproc) -l$(nproc) ARCH=arm64 O=out \
 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- \
 CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image.gz-dtb ]] ; then
-    curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" -d chat_id="1385092591" -d text="Test Failed, Please fix it now @fadlyas07!"
-    curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id="1385092591"
+    curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" -d chat_id=${channel_id} -d text="Test Failed, Please fix it now @fadlyas07!"
+    curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${channel_id}
   exit 1 ;
 fi
-curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id="1385092591"
+curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${channel_id}
 mv $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel-3
 cd $(pwd)/anykernel-3 && zip -r9 GreenForce-"$codename"-"$(TZ=Asia/Jakarta date +'%d%m%y')".zip *
-cd .. && curl -F document=@$(echo $(pwd)/anykernel-3/*.zip) "https://api.telegram.org/bot${token}/sendDocument" -F caption="Test Build $(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3) for #${codename} success at commit $(git log --pretty=format:"%h (\"%s\")" -1)" -F chat_id="1385092591"
+cd .. && curl -F document=@$(echo $(pwd)/anykernel-3/*.zip) "https://api.telegram.org/bot${token}/sendDocument" -F caption="New build for #${codename}, on Linux $(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3) success at commit $(git log --pretty=format:"%h (\"%s\")" -1)" -F chat_id=${channel_id}
 rm -rf out $(pwd)/anykernel-3/*.zip $(pwd)/anykernel-3/zImage
