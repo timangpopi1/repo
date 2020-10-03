8f63762fb3f2e3a1f0e0ea84c547e55c9eff5c66
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2020-10-03 19:46:18 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 git clone --quiet --depth=1 https://github.com/fadlyas07/anykernel-3
 export ARCH=arm64 && export SUBARCH=arm64
-trigger_sha="$(echo $(git rev-parse HEAD) | cut -c 1-8)" && commit_msg="$(git log --pretty=format:'%s' -1)"
+trigger_sha="$(git rev-parse HEAD)" && commit_msg="$(git log --pretty=format:'%s' -1)"
 my_id="1385092591" && channel_id="-1001482549527" && token="1355238694:AAElWMuJhDKoE9Ci6INuD86RXwpo84uTt7c"
 if [[ "$2" == "clang" ]] ; then
-    git clone --quiet --depth=1 https://github.com/greenforce-project/clang-11.0.0 gf
+    git clone --quiet --depth=1 https://github.com/kdrag0n/proton-clang
     function build_now() {
-        export PATH="$(pwd)/gf/bin:$PATH"
-        export LD_LIBRARY_PATH="$(pwd)/gf/lib:$LD_LIBRARY_PATH"
+        export PATH="$(pwd)/proton-clang/bin:$PATH"
         make -j$(nproc) -l$(nproc) ARCH=arm64 O=out \
         CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump \
         CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- STRIP=llvm-strip
@@ -33,7 +32,7 @@ if [[ ! -f $(pwd)/out/arch/arm64/boot/Image.gz-dtb ]] ; then
 fi
 curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
 mv $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel-3
-cd $(pwd)/anykernel-3 && zip -r9 "${KBUILD_BUILD_USER}"-"${KBUILD_BUILD_HOST}"-"${codename}"-"$(TZ=Asia/Jakarta date +'%d%m%y')".zip *
+cd $(pwd)/anykernel-3 && zip -r9q "${KBUILD_BUILD_USER}"-"${KBUILD_BUILD_HOST}"-"${codename}"-"$(TZ=Asia/Jakarta date +'%d%m%y')".zip *
 cd .. && curl -F "disable_web_page_preview=true" -F "parse_mode=html" -F document=@$(echo $(pwd)/anykernel-3/*.zip) "https://api.telegram.org/bot${token}/sendDocument" -F caption="
-New build for #${codename} <b>+</b> $(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3) success at commit <a href='${my_project}/${target_repo}/commit/${trigger_sha}'>${trigger_sha}</a> (\"${commit_msg}\") | <b>SHA1:</b> <code>$(sha1sum $(echo $(pwd)/anykernel-3/*.zip ) | awk '{ print $1 }')</code>." -F chat_id=${channel_id}
+New build for #${codename} <b>+</b> $(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3) success at commit $(echo ${trigger_sha} | cut -c 1-8) (\"<a href='${my_project}/${target_repo}/commit/${trigger_sha}'>${commit_msg}</a>\") | <b>SHA1:</b> <code>$(sha1sum $(echo $(pwd)/anykernel-3/*.zip ) | awk '{ print $1 }')</code>." -F chat_id=${channel_id}
 rm -rf out $(pwd)/anykernel-3/*.zip $(pwd)/anykernel-3/zImage $(pwd)/*.log
