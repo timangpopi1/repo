c1ab5877bf717d8a070d3bca57f8f83311597145
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2020-11-23 21:10:33 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ export ARCH=arm64 && export SUBARCH=arm64
 trigger_sha="$(git rev-parse HEAD)" && commit_msg="$(git log --pretty=format:'%s' -1)"
 my_id="1201257517" && channel_id="-1001407534543" && token="1199423040:AAFES9WZoMa81J8MwA9C1B_F3wqpKByXFA0"
 if [[ "$2" == "clang" ]] ; then
-    git clone --quiet --depth=1 https://github.com/NusantaraDevs/clang 12
+    git clone --quiet --depth=1 https://github.com/kdrag0n/proton-clang
     function build_now() {
-        export PATH="$(pwd)/12/bin:$PATH"
+        export PATH="$(pwd)/proton-clang/bin:$PATH"
         make -j$(nproc) -l$(nproc) ARCH=arm64 O=out \
         CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump \
         CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- STRIP=llvm-strip
