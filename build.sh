17fa4ddb087ca52b11c236aad3a84cf8938b012f
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2020-11-29 19:30:18 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,15 @@ git clone --quiet --depth=1 https://github.com/fadlyas07/anykernel-3
 export ARCH=arm64 && export SUBARCH=arm64
 my_id="1201257517" && channel_id="-1001407534543" && token="1199423040:AAFES9WZoMa81J8MwA9C1B_F3wqpKByXFA0"
 if [[ "$2" == "clang" ]] ; then
-    git clone --quiet --depth=1 https://github.com/kdrag0n/proton-clang
+    git clone --quiet --depth=1 https://github.com/ThankYouMario/proprietary_vendor_qcom_sdclang-8.0_linux-x86 -b quartz quartz
+    git clone --quiet --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r50 gcc
+    git clone --quiet --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r50 gcc32
     function build_now() {
-        export PATH="$(pwd)/proton-clang/bin:$PATH"
+        export PATH="$(pwd)/quartz/bin:$(pwd)/gcc/bin:$(pwd)/gcc32/bin:$PATH"
+        export LD_LIBRARY_PATH="$(pwd)/quartz/lib:$LD_LIBRARY_PATH"
         make -j$(nproc) -l$(nproc) ARCH=arm64 O=out \
-        CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump \
-        CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- STRIP=llvm-strip
+        CC=clang CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
     }
 elif [[ "$2" == "gcc" ]] ; then
     git clone --quiet --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r50 gcc
@@ -36,12 +39,6 @@ if [[ ! -f $(pwd)/out/arch/arm64/boot/Image.gz-dtb ]] ; then
     curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" -d chat_id=${my_id} -d text="Build failed! at branch $(git rev-parse --abbrev-ref HEAD)"
   exit 1 ;
 fi
-make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_now
-make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_now
-make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_now
-make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_now
-make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_now
-make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_now &> build.log
 curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
 mv $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel-3
 cd $(pwd)/anykernel-3 && zip -r9q "${KBUILD_BUILD_USER}"-"${KBUILD_BUILD_HOST}"-"${codename}"-"$(TZ=Asia/Jakarta date +'%d%m%y')".zip *
