a00fe7f231a898c35af2d7dd645d65b30c024709
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2020-12-25 18:41:21 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 git clone --quiet --depth=1 https://github.com/fadlyas07/anykernel-3
 export ARCH=arm64 && export SUBARCH=arm64
 my_id="1201257517" && channel_id="-1001407534543" && token="1199423040:AAFES9WZoMa81J8MwA9C1B_F3wqpKByXFA0"
-if [[ ${msm_test} == 2 ]] ; then
-    wget https://github.com/timangpopi1/scripts-build/raw/master/no_teachpack.patch
-    git apply no_teachpack.patch
-fi
 if [[ "$2" == "clang" ]] ; then
     git clone --quiet --depth=1 https://github.com/KudProject/prebuilts_clang_host_linux-x86 -b r399163b quartz
     git clone --quiet --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r50 gcc
