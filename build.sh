fd3cc91d5d133fb74b30f4badbd5d3f0fc76f877
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2020-12-29 20:49:49 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,12 @@ git clone --quiet --depth=1 https://github.com/fadlyas07/anykernel-3
 export ARCH=arm64 && export SUBARCH=arm64
 my_id="1201257517" && channel_id="-1001407534543" && token="1199423040:AAFES9WZoMa81J8MwA9C1B_F3wqpKByXFA0"
 if [[ "$2" == "clang" ]] ; then
-    git clone --quiet --depth=1 https://github.com/KudProject/prebuilts_clang_host_linux-x86 -b r399163b quartz
-    git clone --quiet --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r50 gcc
-    git clone --quiet --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r50 gcc32
+    git clone --quiet --depth=1 https://github.com/kdrag0n/proton-clang proton
     function build_now() {
-        export PATH="$(pwd)/quartz/bin:$(pwd)/gcc/bin:$(pwd)/gcc32/bin:$PATH"
-        export LD_LIBRARY_PATH="$(pwd)/quartz/lib:$LD_LIBRARY_PATH"
+        export PATH="$(pwd)/proton/bin:$PATH"
         make -j$(nproc) -l$(nproc) ARCH=arm64 O=out \
-        CC=clang CLANG_TRIPLE=aarch64-linux-gnu- \
-        CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
+        CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump \
+        CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- STRIP=llvm-strip
     }
 elif [[ "$2" == "gcc" ]] ; then
     git clone --quiet --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 -b lineage-18.0 gcc
