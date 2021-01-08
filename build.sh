82ac62ca259a4e4ad26e5b24d3b4294d8d0c6e66
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-01-08 19:07:15 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@ git clone --quiet --depth=1 https://github.com/fadlyas07/anykernel-3
 export ARCH=arm64 && export SUBARCH=arm64
 my_id="1201257517" && channel_id="-1001407534543" && token="1199423040:AAFES9WZoMa81J8MwA9C1B_F3wqpKByXFA0"
 if [[ "$2" == "clang" ]] ; then
-    git clone --quiet --depth=1 https://github.com/kdrag0n/proton-clang proton
+    wget -O llvm_project.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.1/clang+llvm-11.0.1-x86_64-linux-gnu-ubuntu-20.10.tar.xz
+    tar -xvf llvm_project.tar.xz && rm -rf llvm_project.tar.xz
+    mv clang* llvm-release
+    git clone --quiet --depth=1 https://github.com/chips-project/aarch64-linux-gnu gcc
+    git clone --quiet --depth=1 https://github.com/chips-project/arm-linux-gnueabi gcc32
     function build_now() {
-        export PATH="$(pwd)/proton/bin:$PATH"
+        export PATH="$(pwd)/llvm-release/bin:$(pwd)/gcc/bin:$(pwd)/gcc32/bin:$PATH"
         make -j$(nproc) -l$(nproc) ARCH=arm64 O=out \
         CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump \
         CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- STRIP=llvm-strip
