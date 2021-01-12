76b1ef769b07197e5f20de6b42a4db818a6985af
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-01-13 06:34:18 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 git clone --quiet --depth=1 https://github.com/fadlyas07/anykernel-3
 export ARCH=arm64 && export SUBARCH=arm64
-my_id="1201257517" && channel_id="-1001407534543" && token="1199423040:AAFES9WZoMa81J8MwA9C1B_F3wqpKByXFA0"
+my_id="1201257517" && channel_id="1201257517" && token="1199423040:AAFES9WZoMa81J8MwA9C1B_F3wqpKByXFA0"
 if [[ "$2" == "clang" ]] ; then
     git clone --quiet --depth=1 https://github.com/kdrag0n/proton-clang
     function build_now() {
