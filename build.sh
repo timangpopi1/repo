1362fdf1cb99c6e87b5910ba4f0eeebf1a4c84be
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-01-17 06:58:23 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 git clone --quiet --depth=1 https://github.com/fadlyas07/anykernel-3
 export ARCH=arm64 && export SUBARCH=arm64
-my_id="1201257517" && channel_id="1201257517" && token="1199423040:AAFES9WZoMa81J8MwA9C1B_F3wqpKByXFA0"
+my_id="1201257517" && channel_id="-1001407534543" && token="1199423040:AAFES9WZoMa81J8MwA9C1B_F3wqpKByXFA0"
 if [[ "$2" == "clang" ]] ; then
     git clone --quiet --depth=1 https://github.com/kdrag0n/proton-clang
     function build_now() {
         export PATH="$(pwd)/proton-clang/bin:$PATH"
         make -j$(nproc) -l$(nproc) ARCH=arm64 O=out \
         CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump \
-        CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- STRIP=llvm-strip
+        CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- STRIP=llvm-strip
     }
 elif [[ "$2" == "gcc" ]] ; then
-    git clone --quiet --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 -b lineage-17.1 gcc
-    git clone --quiet --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 -b lineage-17.1 gcc32
+    wget https://releases.linaro.org/components/toolchain/binaries/4.9-2016.02/aarch64-elf/gcc-linaro-4.9-2016.02-x86_64_aarch64-elf.tar.xz
+    mv gcc-linaro-4.9-2016.02-x86_64_aarch64-elf gcc-elf && rm -rf *.tar.xz
     function build_now() {
-        export PATH="$(pwd)/gcc/bin:$(pwd)/gcc32/bin:$PATH"
+        export PATH="$(pwd)/gcc-elf/bin:$PATH"
         make -j$(nproc) -l$(nproc) ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
     }
 else
