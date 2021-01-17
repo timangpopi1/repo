2269cc550dc50f2592b4d969c68cc1871096f1d3
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-01-17 10:06:01 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ if [[ "$2" == "clang" ]] ; then
         CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- STRIP=llvm-strip
     }
 elif [[ "$2" == "gcc" ]] ; then
-    git clone --quiet --depth=1 https://github.com/timangpopi1/arm64-gcc-linaro-4.9-2016 -b master gcc
+    git clone --quiet --depth=1 https://github.com/timangpopi1/arm64-gcc-linaro-4.9-2016 -b master gcc-elf
     function build_now() {
-        export PATH="$(pwd)/gcc/bin:$PATH"
+        export PATH="$(pwd)/gcc-elf/bin:$PATH"
         make -j$(nproc) -l$(nproc) ARCH=arm64 O=out CROSS_COMPILE=aarch64-elf-
     }
 else
