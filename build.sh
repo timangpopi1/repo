ad27d5876d4491d43b9fcae438961ec6a194d7b6
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-03-25 10:17:33 +0700

    Create build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 git clone --quiet --depth=1 https://github.com/fadlyas07/anykernel-3
 export ARCH=arm64 && export SUBARCH=arm64
-my_id="1201257517" && channel_id="-1001407534543" && token="1747626567:AAGNA9W3HHHVRoaT8bpUpcy65GbK035t4aI"
+my_id="1201257517" && channel_id="1201257517" && token="1747626567:AAGNA9W3HHHVRoaT8bpUpcy65GbK035t4aI"
 if [[ "$2" == "clang" ]] ; then
     git clone --quiet --depth=1 https://github.com/Nicklas373/aosp-clang
     git clone --quiet --depth=1 https://github.com/arter97/arm64-gcc
