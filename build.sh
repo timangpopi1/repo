c64223238298f5f6383512973bed8a300b05f3a2
fadlyas07 <fadlyardhians@gmail.com>
2021-05-01 13:50:34 +0000

    move to gcc

--- build.sh
+++ build.sh
@@ -3,16 +3,14 @@ git clone --quiet --depth=1 https://github.com/fadlyas07/anykernel-3
 export ARCH=arm64 && export SUBARCH=arm64
 my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcshDwfA2x6Q0lhotZT2M-CMeiBJ1U"
 export KBUILD_BUILD_USER=Source.$(git rev-parse --abbrev-ref HEAD) && export KBUILD_BUILD_HOST=$(git log --pretty=format:'%T' -1 | cut -b 1-16)
-function build_clang() {
-    git clone --quiet --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-5799447 clang-9
-    git clone --quiet --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r45 gcc
-    git clone --quiet --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r45 gcc32
-    export PATH="$(pwd)/clang-9/bin:$(pwd)/gcc/bin:$(pwd)/gcc32/bin:$PATH"
-    export LD_LIBRARY_PATH="$(pwd)/clang-9/lib:$LD_LIBRARY_PATH"
-    make -j$(nproc) -l$(nproc) ARCH=arm64 O=out CC=clang CLANG_TRIPLE=aarch64-linux-gnu- \
-    CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
+function build_gcc() {
+    git clone --quiet --depth=1 https://github.com/arter97/arm64-gcc gcc
+    git clone --quiet --depth=1 https://github.com/arter97/arm32-gcc gcc32
+    export PATH="$(pwd)/gcc/bin:$(pwd)/gcc32/bin:$PATH"
+    make -j$(nproc) -l$(nproc) ARCH=arm64 O=out \
+    CROSS_COMPILE=aarch64-elf- CROSS_COMPILE_ARM32=arm-eabi-
 }
-make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_clang 2>&1| tee build.log
+make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_gcc 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image.gz-dtb ]] ; then
     curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
     curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" -d chat_id=${my_id} -d text="Build failed! at branch $(git rev-parse --abbrev-ref HEAD)"
