e2446d178d3ed137c06e6c8b043aec874e648075
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-05-31 05:42:28 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcsh
 export TZ="Asia/Jakarta" && KBUILD_BUILD_TIMESTAMP=$(date) && export KBUILD_BUILD_TIMESTAMP
 export KBUILD_BUILD_USER=fadlyas07.greenforce-project && export KBUILD_BUILD_HOST=$(git log --pretty=format:'%T' -1 | cut -b 1-16)
 build_kernel() {
-    git clone --quiet --depth=1 https://github.com/najahiiii/aarch64-linux-gnu -b gcc8-201903-A gcc
+    git clone --quiet --depth=1 https://github.com/arter97/arm64-gcc gcc
     export PATH="$(pwd)/gcc/bin:$PATH"
-    make -j$(nproc) -l$(nproc) ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-gnu-
+    make -j$(nproc) -l$(nproc) ARCH=arm64 O=out CROSS_COMPILE=aarch64-elf-
 }
 make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_kernel 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image ]] ; then
