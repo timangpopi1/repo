109233a09ac52a3e2a5c2fd86177d9c820b17dc0
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-05-30 20:02:30 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 git clone --quiet --depth=1 https://github.com/fadlyas07/anykernel-3
 export ARCH=arm64 && export SUBARCH=arm64
 my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcshDwfA2x6Q0lhotZT2M-CMeiBJ1U"
-export KBUILD_BUILD_USER=Source.$(git rev-parse --abbrev-ref HEAD) && export KBUILD_BUILD_HOST=$(git log --pretty=format:'%T' -1 | cut -b 1-16)
-build_clang() {
-    git clone --quiet --depth=1 https://github.com/timangpopi1/arm64-gcc-linaro-4.9-2016 -b old gcc
-    git clone --quiet --depth=1 https://github.com/arter97/arm-eabi-5.1.git -b master gcc32
-    export PATH="$(pwd)/gcc/bin:$(pwd)/gcc32/bin:$PATH"
-    make -j$(nproc) -l$(nproc) ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-eabi-
+export TZ="Asia/Jakarta" && KBUILD_BUILD_TIMESTAMP=$(date) && export KBUILD_BUILD_TIMESTAMP
+export KBUILD_BUILD_USER=fadlyas07.greenforce-project && export KBUILD_BUILD_HOST=$(git log --pretty=format:'%T' -1 | cut -b 1-16)
+build_kernel() {
+    git clone --quiet --depth=1 https://github.com/najahiiii/aarch64-linux-gnu -b gcc8-201903-A gcc
+    export PATH="$(pwd)/gcc/bin:$PATH"
+    make -j$(nproc) -l$(nproc) ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-gnu-
 }
-make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_clang 2>&1| tee build.log
+make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_kernel 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image.gz-dtb ]] ; then
     curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
     curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" -d chat_id=${my_id} -d text="Build failed! at branch $(git rev-parse --abbrev-ref HEAD)"
@@ -18,7 +18,7 @@ fi
 if [[ $codename == lavender ]] ; then
     export DEVICE="Xiaomi Redmi Note 7/7S"
 elif [[ $codename == juice ]] ; then
-    export DEVICE="POCO M3/Redmi 9T"
+    export DEVICE="POCO M3 & Redmi 9T"
 fi
 curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
 mv $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel-3
