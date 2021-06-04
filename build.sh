9774cbdd5b91112aa5d9fa660ea4496fd4585e29
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-06-05 05:27:24 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ export ARCH=arm64 && export SUBARCH=arm64
 my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcshDwfA2x6Q0lhotZT2M-CMeiBJ1U"
 export KBUILD_BUILD_USER=fadlyas07.greenforce-project && export KBUILD_BUILD_HOST=$(git log --pretty=format:'%T' -1 | cut -b 1-16)
 build_kernel() {
-    git clone --quiet -j64 --depth=1 -b master https://github.com/timangpopi1/arm32-gcc
-    git clone --quiet -j64 --depth=1 -b master https://github.com/timangpopi1/arm64-gcc
-    export PATH="$(pwd)/arm64-gcc/bin:$(pwd)/arm32-gcc/bin;$PATH"
+    git clone --quiet -j64 --depth=1 -b gcc8-201903-A https://github.com/najahiiii/aarch64-linux-gnu.git gcc
+    git clone --quiet -j64 --depth=1 -b master https://github.com/arter97/arm-eabi-5.1.git gcc32
+    export PATH="$(pwd)/gcc/bin:$(pwd)/gcc32/bin;$PATH"
     make -j$(nproc) -l$(nproc) ARCH=arm64 O=out \
     CROSS_COMPILE=aarch64-elf- CROSS_COMPILE_ARM32=arm-eabi-
 }
