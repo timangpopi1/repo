ce9adb6bb23f7130072e636dfb15b09ce254c117
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-06-05 05:15:32 +0700

    Create build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,11 @@ export ARCH=arm64 && export SUBARCH=arm64
 my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcshDwfA2x6Q0lhotZT2M-CMeiBJ1U"
 export KBUILD_BUILD_USER=fadlyas07.greenforce-project && export KBUILD_BUILD_HOST=$(git log --pretty=format:'%T' -1 | cut -b 1-16)
 build_kernel() {
-    git clone --quiet --depth=1 https://github.com/greenforce-project/clang-llvm
-    export PATH="$(pwd)/clang-llvm/bin:$PATH"
-    export LD_LIBRARY_PATH="$(pwd)/clang-llvm/lib:$LD_LIBRARY_PATH"
+    git clone --quiet -j64 --depth=1 -b master https://github.com/timangpopi1/arm32-gcc
+    git clone --quiet -j64 --depth=1 -b master https://github.com/timangpopi1/arm64-gcc
+    export PATH="$(pwd)/arm64-gcc/bin:$(pwd)/arm32-gcc/bin;$PATH"
     make -j$(nproc) -l$(nproc) ARCH=arm64 O=out \
-    CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump \
-    CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- STRIP=llvm-strip
+    CROSS_COMPILE=aarch64-elf- CROSS_COMPILE_ARM32=arm-eabi-
 }
 make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_kernel 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image ]] ; then
@@ -20,7 +19,7 @@ fi
 if [[ $codename == lavender ]] ; then
     export DEVICE="Redmi Note 7/7S"
 elif [[ $codename == juice ]] ; then
-    export DEVICE="POCO M3/Redmi 9T/Redmi Note 9 (4G)"
+    export DEVICE="POCO M3/Redmi 9T"
 fi
 curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
 mv $(pwd)/out/arch/arm64/boot/Image $(pwd)/anykernel-3
