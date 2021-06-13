309718d54759f5b42e6403871415700c9a06ea15
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-06-13 20:25:43 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ export ARCH=arm64 && export SUBARCH=arm64
 my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcshDwfA2x6Q0lhotZT2M-CMeiBJ1U"
 export KBUILD_BUILD_USER=fadlyas07.greenforce-project && export KBUILD_BUILD_HOST=$(git log --pretty=format:'%T' -1 | cut -b 1-16)
 build_kernel() {
-    git clone --quiet -j64 --depth=1 -b master https://github.com/arter97/arm64-gcc gcc
-    export PATH="$(pwd)/gcc/bin:$PATH"
+    git clone --quiet -j64 --depth=1 -b master https://github.com/timangpopi1/arm64-gcc
+    export PATH="$(pwd)/arm64-gcc/bin:$PATH"
     make -j$(nproc) -l$(nproc) ARCH=arm64 O=out CROSS_COMPILE=aarch64-elf-
 }
 make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_kernel 2>&1| tee build.log
