54dcaba5f8a544beb39c6652562e042ce4eb5ed7
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-06-14 06:19:48 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcsh
 export KBUILD_BUILD_USER=fadlyas07.greenforce-project && export KBUILD_BUILD_HOST=$(git log --pretty=format:'%T' -1 | cut -b 1-16)
 build_kernel() {
     git clone --quiet -j64 --depth=1 -b master https://github.com/timangpopi1/arm64-gcc
-    export PATH="$(pwd)/arm64-gcc/bin:$PATH"
-    make -j$(nproc) -l$(nproc) ARCH=arm64 O=out CROSS_COMPILE=aarch64-elf-
+    git clone --quiet -j64 --depth=1 -b master https://github.com/timangpopi1/arm32-gcc
+    export PATH="$(pwd)/arm64-gcc/bin:(pwd)/arm32-gcc/bin:$PATH"
+    make -j$(nproc) -l$(nproc) ARCH=arm64 O=out CROSS_COMPILE=aarch64-elf- CROSS_COMPILE_ARM32=arm-eabi-
 }
 make -j$(nproc) -l$(nproc) ARCH=arm64 O=out ${1} && build_kernel 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image ]] ; then
