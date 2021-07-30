edf817facc7a6af2c1dfa4621a5918b365d018e8
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-07-30 21:50:13 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-git clone --quiet -j64 --depth=1 https://github.com/fadlyas07/anykernel-3
+git clone --quiet -j64 --depth=1 --single-branch https://github.com/fadlyas07/anykernel-3
+git clone --quiet -j64 --depth=1 --single-branch https://gitlab.com/najahi/clang
 export ARCH=arm64 && export SUBARCH=arm64 && export kernel_defconfig=${1}
 my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcshDwfA2x6Q0lhotZT2M-CMeiBJ1U"
-export KBUILD_BUILD_USER=fadlyas07 && export KBUILD_BUILD_HOST=greenforce-project
-build_kernel() {
-    git clone --quiet -j64 --depth=1 --single-branch https://github.com/greenforce-project/clang-llvm clang
-    export PATH="$(pwd)/clang/bin:$PATH" && export LD_LIBRARY_PATH="$(pwd)/clang/lib:$LD_LIBRARY_PATH"
-    make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out CC=clang CROSS_COMPILE=aarch64-linux-gnu-
-}
-make -j$(nproc) -l$(nproc) ARCH=arm64 O=out $kernel_defconfig
+export PATH="$(pwd)/clang/bin:$PATH" && export KBUILD_BUILD_USER=fadlyas07 && export KBUILD_BUILD_HOST=greenforce-project
+make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out CC=clang CROSS_COMPILE=aarch64-linux-gnu- $kernel_defconfig
 #rm -rf $(pwd)/out/.config && cp $(pwd)/arch/arm64/configs/$kernel_defconfig $(pwd)/out/.config
-build_kernel 2>&1| tee build.log
+make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out CC=clang CROSS_COMPILE=aarch64-linux-gnu- 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image ]] ; then
     curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
     curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" -d chat_id=${my_id} -d text="Build failed! at branch $(git rev-parse --abbrev-ref HEAD)"
