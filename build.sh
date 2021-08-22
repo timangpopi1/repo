5489a7f61448369d519a1534235cfeb14385a23f
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-08-23 05:30:16 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ git clone --quiet -j64 --depth=1 --single-branch https://github.com/greenforce-p
 export ARCH=arm64 && export SUBARCH=arm64 && export kernel_defconfig=${1}
 my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcshDwfA2x6Q0lhotZT2M-CMeiBJ1U"
 export PATH="$(pwd)/clang/bin:$PATH" && export KBUILD_BUILD_USER="y.z" && export KBUILD_BUILD_HOST=Gf-Lab
-make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out AR=llvm-ar CC=clang CROSS_COMPILE=aarch64-linux-gnu- NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump LD=ld.lld STRIP=llvm-strip $kernel_defconfig
-make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out AR=llvm-ar CC=clang CROSS_COMPILE=aarch64-linux-gnu- CONFIG_TOOLS_SUPPORT_RELR=y CONFIG_RELR=y NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump LD=ld.lld STRIP=llvm-strip 2>&1| tee build.log
+make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out $kernel_defconfig
+make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out CC=clang CROSS_COMPILE=aarch64-linux-gnu- 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image.gz-dtb ]] ; then
     curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
     curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" -d chat_id=${my_id} -d text="Build failed! at branch $(git rev-parse --abbrev-ref HEAD)"
@@ -28,5 +28,5 @@ curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDoc
 cd $(pwd)/anykernel-3 && zip -r9q "${2}"-"${codename}"-"$(TZ=Asia/Jakarta date +'%d%m%y')".zip *
 cd .. && curl -F "disable_web_page_preview=true" -F "parse_mode=html" -F document=@$(echo $(pwd)/anykernel-3/*.zip) "https://api.telegram.org/bot${token}/sendDocument" -F caption="
 New updates for <b>$DEVICE</b> based on Linux <b>$(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3)</b> at commit $(git log --pretty=format:"%h (\"%s\")" -1) | <b>SHA1:</b> $(sha1sum "$(echo $(pwd)/anykernel-3/*.zip)" | awk '{ print $1 }')" -F chat_id=${channel_id}
-make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out AR=llvm-ar CC=clang CROSS_COMPILE=aarch64-linux-gnu- NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump LD=ld.lld STRIP=llvm-strip $kernel_defconfig && make savedefconfig
+make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out CC=clang CROSS_COMPILE=aarch64-linux-gnu- $kernel_defconfig && make savedefconfig
 curl -F document=@$(pwd)/defconfig "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
