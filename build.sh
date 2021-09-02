ae0e383672a1cc8f6a495bdceff98d8ba2b56cf7
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-09-02 18:45:46 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ git clone --quiet -j64 --depth=1 --single-branch https://github.com/kdrag0n/prot
 export ARCH=arm64 && export SUBARCH=arm64 && export kernel_defconfig=${1}
 my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcshDwfA2x6Q0lhotZT2M-CMeiBJ1U"
 export PATH="$(pwd)/clang/bin:$PATH" && export KBUILD_BUILD_USER="y.z" && export KBUILD_BUILD_HOST=Gf-Lab
-kmake_flags+=(AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld)
-make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out CC=clang "kmake_flags" CROSS_COMPILE=aarch64-linux-gnu- $kernel_defconfig
-make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out CC=clang "kmake_flags" CROSS_COMPILE=aarch64-linux-gnu- 2>&1| tee build.log
+cc_flags="AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld"
+make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out CC=clang "cc_flags" CROSS_COMPILE=aarch64-linux-gnu- $kernel_defconfig
+make -j$(nproc --all) -l$(nproc --all) ARCH=arm64 O=out CC=clang "cc_flags" CROSS_COMPILE=aarch64-linux-gnu- 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image.gz-dtb ]] ; then
     curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
     curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" -d chat_id=${my_id} -d text="Build failed! at branch $(git rev-parse --abbrev-ref HEAD)"
