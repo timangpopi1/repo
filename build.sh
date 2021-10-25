16e052e725286b0c29e0321ec018bc6254a78666
ubuntu <ubuntu@ubuntu>
2021-10-25 09:22:13 +0000

    wen
    Change-Id: Id084cf650521c2986486a1d56eafa416f9279af1

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-git clone --quiet -j64 --depth=1 --single-branch https://github.com/greenforce-project/aosp_clang clang
-git clone --quiet -j64 --depth=1 --single-branch https://github.com/greenforce-project/gcc-arm64 gcc
-git clone --quiet -j64 --depth=1 --single-branch https://github.com/greenforce-project/gcc-arm32 gcc32
+git clone --quiet -j64 --depth=1 -b old https://github.com/timangpopi1/arm64-gcc-linaro-4.9-2016 gcc
 git clone --quiet -j64 --depth=1 --single-branch https://github.com/fadlyas07/anykernel-3
 export ARCH=arm64 && export SUBARCH=arm64 && export kernel_defconfig=${1}
 my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcshDwfA2x6Q0lhotZT2M-CMeiBJ1U"
-export PATH="$(pwd)/clang/bin:$(pwd)/gcc/bin:$(pwd)/gcc32/bin:${PATH}"
-export KBUILD_BUILD_USER="ubuntu" && export KBUILD_BUILD_HOST="127.0.0.1"
-BUILD_ENV="ARCH=arm64 CROSS_COMPILE=aarch64-elf- CROSS_COMPILE_COMPAT=arm-eabi- CLANG_TRIPLE=aarch64-linux-gnu-"
+export PATH="$(pwd)/gcc/bin:${PATH}"
+export KBUILD_BUILD_USER="" && export KBUILD_BUILD_HOST=""
+BUILD_ENV="ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-"
 make -j$(nproc --all) -C $(pwd) O=$(pwd)/out $BUILD_ENV $kernel_defconfig || exit 1
 make -j$(nproc --all) -C $(pwd) O=$(pwd)/out $BUILD_ENV 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image ]] ; then
