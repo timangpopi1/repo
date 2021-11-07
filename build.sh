beac0f5165ef6cb31a8dbd350340a99305aefabe
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-11-07 09:35:06 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-git clone --quiet -j64 --depth=1 --single-branch https://github.com/greenforce-project/gcc-arm64
+git clone --quiet -j64 --depth=1 --single-branch https://github.com/timangpopi1/clang
 git clone --quiet -j64 --depth=1 --single-branch https://github.com/fadlyas07/anykernel-3
-export ARCH=arm64 && export SUBARCH=arm64 && export kernel_defconfig=${1}
+export ARCH=arm64 && export SUBARCH=arm64 && export kernel_defconfig=${1} && thread=$(nproc --all)
 my_id="1201257517" && channel_id="-1001360920692" && token="1501859780:AAFrTzcshDwfA2x6Q0lhotZT2M-CMeiBJ1U"
-export PATH="$(pwd)/gcc-arm64/bin:$PATH" export KBUILD_BUILD_USER="" && export KBUILD_BUILD_HOST=""
-BUILD_ENV="ARCH=arm64 CROSS_COMPILE=aarch64-elf- CONFIG_LTO_GCC=y"
-make -j$(nproc --all) -C $(pwd) O=$(pwd)/out $BUILD_ENV $kernel_defconfig || exit 1
-make -j$(nproc --all) -C $(pwd) O=$(pwd)/out $BUILD_ENV 2>&1| tee build.log
+export PATH="$(pwd)/clang/bin:$PATH" export KBUILD_BUILD_USER="" && export KBUILD_BUILD_HOST=""
+BUILD_ENV="'ARCH=arm64' 'CC=clang' 'CROSS_COMPILE=aarch64-linux-gnu-'"
+make -j${thread} -l${thread} -C $(pwd) O=$(pwd)/out $BUILD_ENV $kernel_defconfig || exit 1;
+make -j${thread} -l${thread} -C $(pwd) O=$(pwd)/out $BUILD_ENV 2>&1| tee build.log
 if [[ ! -f $(pwd)/out/arch/arm64/boot/Image ]] ; then
     curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
     curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" -d chat_id=${my_id} -d text="Build failed! at branch $(git rev-parse --abbrev-ref HEAD)"
@@ -26,6 +26,6 @@ elif [[ $codename == juice ]] ; then
 fi
 mv $(pwd)/out/arch/arm64/boot/Image $(pwd)/anykernel-3
 curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot${token}/sendDocument" -F chat_id=${my_id}
-cd $(pwd)/anykernel-3 && zip -r9q "${2}"-"${codename}"-"$(TZ=Asia/Jakarta date +'%d%m%y')".zip *
+cd $(pwd)/anykernel-3 && zip -r9q "${2}"-"${codename}"-"$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')".zip *
 cd .. && curl -F "disable_web_page_preview=true" -F "parse_mode=html" -F document=@$(echo $(pwd)/anykernel-3/*.zip) "https://api.telegram.org/bot${token}/sendDocument" -F caption="
 New updates for <b>$DEVICE</b> based on Linux <b>$(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3)</b> at commit $(git log --pretty=format:"%h (\"%s\")" -1) | <b>SHA1:</b> $(sha1sum "$(echo $(pwd)/anykernel-3/*.zip)" | awk '{ print $1 }')" -F chat_id=${channel_id}
