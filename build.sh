362094d872d042a6ac3dc51edb9212b8ec7182d6
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2021-11-25 18:54:16 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 git clone -q -j$(nproc --all) --single-branch https://github.com/greenforce-project/clang-llvm --depth=1
 git clone -q -j$(nproc --all) --single-branch https://github.com/fadlyas07/anykernel-3 --depth=1
-export id=${1} && export token=${2} && export c_id=${3} && export KBUILD_BUILD_USER="Y.Z" && export KBUILD_BUILD_HOST="" && PATH=$(pwd)/clang-llvm/bin:$PATH 
-BUILD_ENV="CC=clang ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi-"
-BUILD_ENV="$BUILD_ENV target_defconfig=${4}"
-make -j$(nproc --all) -C $(pwd) O=out $BUILD_ENV $target_defconfig || exit 1
-make -j$(nproc --all) -C $(pwd) O=out $BUILD_ENV 2>&1| tee build.log
+export id=${1} && export token=${2} && export c_id=${3} && export KBUILD_BUILD_USER="Y.Z" && export KBUILD_BUILD_HOST="" 
+export PATH=$(pwd)/clang-llvm/bin:$PATH
+main_env="ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu-"
+make -j$(nproc --all) -C $(pwd) O=out $main_env $target_defconfig || echo "fail to regen defconfig, maybe you put the wrong name of your defconfig!"
+make -j$(nproc --all) -C $(pwd) O=out $main_env 2>&1| tee build.log
 if ! [[ ( -f $(pwd)/out/arch/arm64/boot/Image || $(pwd)/out/arch/arm64/boot/Image.gz-dtb ) ]] ; then
     curl -F document=@$(pwd)/build.log "https://api.telegram.org/bot$token/sendDocument" -F chat_id=$id
     curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id=$id -d text="Build for $(git rev-parse --abbrev-ref HEAD) failed!"
